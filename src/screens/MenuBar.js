07318bd8e29793841daa1b55c814db5748fba261
letsjo <hyunoh.jo@gmail.com>
2023-02-09 13:05:26 +0900

    feat(MenuBar): MenuBar init

---
+++ src/screens/MenuBar.js
@@ -0,0 +1,16 @@
+import { ScrollView, View, Text } from 'react-native'
+import styled from 'styled-components/native';
+
+const MenuBar = () => {
+  return (
+    <View>
+      <ScrollView>
+        <Text>MenuBar</Text>
+        <Text>MenuBar</Text>
+        <Text>MenuBar</Text>
+      </ScrollView>
+    </View>
+  )
+}
+
+export default MenuBar
