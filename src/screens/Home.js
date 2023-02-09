63a78fca74b81947967659bb0efd142997b47f09
letsjo <hyunoh.jo@gmail.com>
2023-02-09 14:02:38 +0900

    feat(Home): products list 추가 작업

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,11 @@
 import { ScrollView, Text } from 'react-native'
 import styled from 'styled-components/native';
 
-const Container = styled.ScrollView`
+import userApi from '../apis/userApi'
+import { useEffect, useState } from 'react';
+import ProductCard from '../components/ProductCard';
+
+const Container = styled.View`
   flex: 24;
   background-color: #f3f3f3;
 `;
@@ -9,67 +13,60 @@ const Container = styled.ScrollView`
 const MenuBtn = styled.Button`
 
 `
+const ProductsWrapper = styled.FlatList`
+  background-color: #fff;
+`
+
+const ProductsCardTouch = styled.TouchableOpacity`
+  padding-left: 10px;
+  padding-right: 10px;
+`
 
 const Home = ({ navigation }) => {
+
+  const [products, setProducts] = useState([]);
+  const [containerWidth, setContainerWidth] = useState(0);
+
+  const margins = 39 * 2;
+  const numColumns = 2;
+
+  useEffect(() => {
+    const getProductList = () => {
+      userApi.get('/products')
+        .then(res => {
+          setProducts(res.data);
+        }).catch(error => console.log(error));
+    }
+    getProductList();
+  }, []);
+
   return (
     <Container>
       <MenuBtn title={'MenuBar'} onPress={() => navigation.navigate('MenuBar')}><Text>124214Home</Text></MenuBtn>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
+      <ProductsWrapper
+        data={products}
+        columnWrapperStyle={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          marginTop: 18,
+          marginBottom: 18,
+        }}
+        onLayout={e => setContainerWidth(e.nativeEvent.layout.width)}
+        renderItem={(product) => (
+          <ProductsCardTouch
+            onPress={() => navigation.navigate('ProductDetail', { product })}
+          >
+            <ProductCard
+              width={(containerWidth - margins) / numColumns}
+              product={product.item}
+            />
+          </ProductsCardTouch>
+        )}
+        keyExtractor={(item, index) => index}
+        numColumns={numColumns}
+      />
     </Container>
   )
 }
