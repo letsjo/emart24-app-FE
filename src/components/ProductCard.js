8d2f6a0587e640169edfae76346ae0e3a25ff3c1
letsjo <hyunoh.jo@gmail.com>
2023-02-13 17:12:16 +0900

    feat(*): 상품 카드 구현

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,49 +1,25 @@
 import React from 'react'
-import styled, { css } from 'styled-components/native';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
 const Container = styled.View`
-  ${({ width }) => {
-    return css`
-      width: ${width};
-    `}
-  }
   backgroundColor: '#f9c2ff';
 `
 
 const TopWrapper = styled.View`
-  ${({ width }) => {
-    return css`
-      width: ${width};
-      height: ${width};
-    `}
-  }
 `
 
 const Image = styled.Image`
-  ${({ width }) => {
-    return css`
-      width: ${width};
-      height: ${width};
-      border-radius: ${width * 0.05}
-    `}
-  }
   object-fit: cover;
 `
 
 const ProductNameWrapper = styled.View`
   position: absolute;
-  top: 10;
-  left: 10;
   padding: 3px;
   background-color: #555;
 `
 
 const ProductName = styled.Text`
-  ${({ width }) => {
-    return css`
-      font-size: ${width * 0.05};
-    `}
-  }
   color: #fff;
   font-weight: bold;
 `
@@ -52,52 +28,72 @@ const BottomWrapper = styled.View`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: flex-start;
+  align-items: center;
 `
 
 const ProductTitle = styled.Text`
-  ${({ width }) => {
-    return css`
-      font-size: ${width * 0.1};
-      font-weight: bold;
-    `}
-  }
+  font-weight: light;
+  text-align: center;
 `
 
 const ProductDescription = styled.Text`
-  ${({ width }) => {
-    return css`
-      font-size: ${width * 0.07};
-      font-weight: bold;
-    `}
-  }
+  font-weight: bold;
 `
 
-function ProductCard({ product, width }) {
+const AddCart = styled.TouchableOpacity`
+  background-color: #ffb71b;
+  justify-content: center;
+  align-items: center;
+`
+
+const ProductPrice = styled.Text`
+  color: red;
+  font-weight: bold;
+`
+
+function ProductCard({ product, scale }) {
   return (
-    <Container width={width}>
+    <Container style={ { width: scale } }>
       <TopWrapper
-        width={width}
+        style={ { width: scale, height: scale } }
       >
         <Image
-          width={width}
+          style={ { width: scale, height: scale, borderRadius: (scale * 0.05) } }
           source={
             { uri: product.thumbnail }
           } />
-        {product.name && (
-          <ProductNameWrapper>
-            <ProductName width={width}>
-              {product.category}
-            </ProductName>
-          </ProductNameWrapper>)
+        {
+          product.name && (
+            <ProductNameWrapper style={ { top: 10, left: 10 } }>
+              <ProductName style={ { fontSize: scale * 0.05 } }>
+                { product.category }
+              </ProductName>
+            </ProductNameWrapper>)
         }
       </TopWrapper>
       <BottomWrapper>
-        <ProductTitle width={width} numberOfLines={2}>{product.name}</ProductTitle>
-        <ProductDescription width={width} numberOfLines={1}>{product.description}</ProductDescription>
+        <ProductTitle style={ { fontSize: scale * 0.07 } } numberOfLines={ 2 }>{ product.name }</ProductTitle>
+        <ProductPrice style={ { fontSize: scale * 0.07 } }>{ product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") } 원</ProductPrice>
+        <AddCart activeOpacity={ 0.8 } style={ { width: scale * 0.5, height: 25, borderRadius: 8 } } onPress={ () => console.log(product.id) }>
+          <Text style={ { fontSize: scale * 0.07 } }>담기</Text>
+        </AddCart>
+        {/* <ProductDescription style={{ fontSize: scale * 0.05 }} numberOfLines={1}>{product.description}</ProductDescription> */ }
       </BottomWrapper>
-    </Container>
+    </Container >
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#ffb71b",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    color: "#fff"
+  }
+});
 
 export default ProductCard
