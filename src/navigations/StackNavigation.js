07b3379bc0fe72fd850b4048bab4c69453cce025
letsjo <hyunoh.jo@gmail.com>
2023-02-14 14:40:11 +0900

    feat(*): drawer 메뉴 프레임 추가

--- src/navigations/StackNavigation.js
+++ src/navigations/StackNavigation.js
@@ -7,107 +7,135 @@ import ProductDetail from "../screens/ProductDetail";
 import Login from "../screens/Login";
 import Signup from "../screens/Signup";
 
+import { useNavigation } from '@react-navigation/native';
+import MyPage from "../screens/MyPage";
+import BranchAuth from "../screens/BranchAuth";
+import { FontAwesome } from '@expo/vector-icons';
+import { useRecoilState } from 'recoil';
+import { CartState } from "../state/CartState";
+import Cart from "../screens/Cart";
+import Event from "../screens/Event";
+import UpdatePassword from "../screens/UpdatePassword";
+import UpdateProfile from "../screens/UpdateProfile";
+
 const Stack = createStackNavigator();
 
-const HeaderLogo = styled.Image`
-
-`;
-
-const MenuBtn = styled.TouchableOpacity`
-
-`
-
-const MenuImage = styled.Image`
-  width: 25PX;
-  height: 25px;
+const IoButtonContainer = styled.TouchableOpacity`
+  position: absolute;
+  right: 20;
+  bottom: 30;
+  width: 60;
+  height: 60;
+  background-color: #ffb916;
+  opacity: 0.9;
+  border-radius: 30;
+  justify-content: center;
+  align-items: center;
+  z-index:1;
+  shadow-color: #222;
+  show-offset: {
+    width: 0;
+    height: 10;
+  }
+  shadow-opacity: 0.3;
+  elevation: 10;
 `;
 
 function StackNavigation() {
+  const navigation = useNavigation();
+  const [isViewCart, setIsViewCart] = useRecoilState(CartState);
+
   return (
-    <Stack.Navigator
-      initialRouteName='Home'
-      screenOptions={ {
-        headerStyle: {
-          height: 100,
-          shadowColor: '#222',
-          shadowOffset: {
-            width: 0,
-            height: 5,
-          },
-          shadowOpacity: 0.2,
-          shadowRadius: 4.65,
-        },
-        headerTitleAlign: 'center',
-        headerTitle: (props) => (
-          <HeaderLogo source={ require('../../assets/logo.png') } style={ { width: 110, height: 21 } } />
-        ),
-      } }
-    >
-      <Stack.Screen
-        name='Home'
-        component={ Home }
-        options={ {
-          headerLeft: () => {
-            const style = {
-              marginRight: 11,
-              marginLeft: 20,
-            };
-            return (
-              <MenuBtn onPress={ () => { console.log('내정보 클릭') } }>
-                <MenuImage
-                  source={ require('../../assets/icons/user.png') }
-                  style={ style }
-                />
-              </MenuBtn>
-            )
-          },
-          headerRight: () => {
-            const style = {
-              marginRight: 20,
-              marginLeft: 11,
-            };
-            return (
-              <MenuBtn onPress={ () => navigation.navigate('MenuBar') }>
-                <MenuImage
-                  source={ require('../../assets/icons/menu.png') }
-                  style={ style }
-                />
-              </MenuBtn>
-            )
-          },
-        } }
-      />
-      <Stack.Screen
-        name="MenuBar"
-        component={ MenuBar }
-        options={ {
-          ...TransitionPresets.SlideFromRightIOS,
-        } }
-      />
-      <Stack.Screen
-        name="ProductDetail"
-        component={ ProductDetail }
-        options={ {
-          ...TransitionPresets.SlideFromRightIOS,
-        } }
-      />
-      <Stack.Screen
-        name="Login"
-        component={ Login }
-        options={ {
-          ...TransitionPresets.SlideFromRightIOS,
-          gestureDirection: 'horizontal-inverted',
-        } }
-      />
-      <Stack.Screen
-        name="Signup"
-        component={ Signup }
-        options={ {
+    <>
+      <IoButtonContainer style={ { display: isViewCart } } onPress={ () => navigation.navigate('Cart') }>
+        <FontAwesome name="shopping-cart" size={ 24 } color="#fff" />
+      </IoButtonContainer>
+      <Stack.Navigator
+        initialRouteName='Home'
+        screenOptions={ {
           headerShown: false,
-          ...TransitionPresets.ModalPresentationIOS,
         } }
-      />
-    </Stack.Navigator>
+      >
+        <Stack.Screen
+          name='Home'
+          component={ Home }
+        />
+        <Stack.Screen
+          name="MenuBar"
+          component={ MenuBar }
+          options={ {
+            ...TransitionPresets.SlideFromRightIOS,
+          } }
+        />
+        <Stack.Screen
+          name="ProductDetail"
+          component={ ProductDetail }
+          options={ {
+            ...TransitionPresets.SlideFromRightIOS,
+          } }
+        />
+        <Stack.Screen
+          name="Cart"
+          component={ Cart }
+          options={ {
+            ...TransitionPresets.SlideFromRightIOS,
+          } }
+        />
+        <Stack.Screen
+          name="Event"
+          component={ Event }
+          options={ {
+            ...TransitionPresets.SlideFromRightIOS,
+          } }
+        />
+        <Stack.Screen
+          name="Login"
+          component={ Login }
+          options={ {
+            ...TransitionPresets.SlideFromRightIOS,
+            gestureDirection: 'horizontal-inverted',
+          } }
+        />
+        <Stack.Screen
+          name="MyPage"
+          component={ MyPage }
+          options={ {
+            ...TransitionPresets.SlideFromRightIOS,
+            gestureDirection: 'horizontal-inverted',
+          } }
+        />
+        <Stack.Screen
+          name="BranchAuth"
+          component={ BranchAuth }
+          options={ {
+            ...TransitionPresets.SlideFromRightIOS,
+            gestureDirection: 'horizontal-inverted',
+          } }
+        />
+        <Stack.Screen
+          name="UpdatePassword"
+          component={ UpdatePassword }
+          options={ {
+            ...TransitionPresets.ModalPresentationIOS,
+          } }
+        />
+        <Stack.Screen
+          name="UpdateProfile"
+          component={ UpdateProfile }
+          options={ {
+            ...TransitionPresets.ModalPresentationIOS,
+          } }
+        />
+        <Stack.Screen
+          name="Signup"
+          component={ Signup }
+          options={ {
+            headerShown: false,
+            ...TransitionPresets.ModalPresentationIOS,
+          } }
+        />
+      </Stack.Navigator>
+    </>
   )
 }
 
